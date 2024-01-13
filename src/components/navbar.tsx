5d02e47346a793aa67b589553dcf0b5e4d723739
Eric Hardy <erichardy011@gmail.com>
2024-01-13 01:21:25 -0600

    basic navbar setup

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,3 +9,13 @@
         Log In
             Firebase auth
 */
+import { Link } from 'react-router-dom'
+export default function Navbar() {
+    return (
+        <nav>
+            <Link to='/'>Home</Link>
+            <Link to='/signup'>Sign Up</Link>
+            <Link to='/login'>Log In</Link>
+        </nav>
+    )
+} 
