270f15e31696ab2c6e49dd5801952450fc4845b3
Eric Hardy <erichardy011@gmail.com>
2024-01-13 00:55:22 -0600

    subfolders for individual pages to help organization

---
+++ src/pages/Login/login.tsx
@@ -0,0 +1,10 @@
+export default function Login() {
+    return (
+        <section className='login-card'>
+            <form className='login-form'>
+                
+            </form>
+        </section>
+    )
+
+}
---
+++ src/pages/Signup/signup.tsx
@@ -0,0 +1,10 @@
+export default function Signup() {
+    return (
+        <section className='signup-card'>
+            <form className='signup-form'>
+                
+            </form>
+        </section>
+    )
+
+}
