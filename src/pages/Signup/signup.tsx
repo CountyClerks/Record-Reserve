9c536ae9c1d2a0aae4601e12d25580c5f4a1198f
Eric Hardy <erichardy011@gmail.com>
2024-01-16 02:27:27 -0600

    sign up input form

--- src/pages/Signup/signup.tsx
+++ src/pages/Signup/signup.tsx
@@ -1,10 +1,25 @@
 export default function Signup() {
     return (
-        <section className='signup-card'>
-            <form className='signup-form'>
-                
-            </form>
-        </section>
+        <main>
+            <section className='signup-card'>
+                <form className='signup-form'>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        className="signup-input"
+                        value={}
+                        required/>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className="signup-input"
+                        value={}
+                        required/>
+                    <button type="submit" className="signup-button">Sign Up</button>
+                </form>
+            </section>
+        </main>
+
     )
 
 }
