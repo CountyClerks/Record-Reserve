afbbd5cf548e09f9bc6ff6f80b98ecff13f1d325
Eric Hardy <erichardy011@gmail.com>
2024-01-16 03:39:51 -0600

    create firebase config

---
+++ src/services/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCkqxsZrSvxHxCBrz3on05eVmCq_oRZ-Mk",
+    authDomain: "record-reserve-f9783.firebaseapp.com",
+    projectId: "record-reserve-f9783",
+    storageBucket: "record-reserve-f9783.appspot.com",
+    messagingSenderId: "575292874176",
+    appId: "1:575292874176:web:6fa3587920fbf3fdfc3c25",
+    measurementId: "G-BLDNL560ST"
+};
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
