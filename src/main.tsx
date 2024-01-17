b45144c9aed97827a488e4aaced07010ffa11c51
Eric Hardy <erichardy011@gmail.com>
2024-01-16 20:07:59 -0600

    add auth context to webapp

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './services/routes.tsx'
+import { AuthUserProvider } from './services/auth.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthUserProvider>
+      <RouterProvider router={router} />
+    </AuthUserProvider>
   </React.StrictMode>,
 )
