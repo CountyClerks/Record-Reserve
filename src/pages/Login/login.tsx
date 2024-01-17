b28fcb1afb07b3929e7e6492d5567367cb8f467b
Eric Hardy <erichardy011@gmail.com>
2024-01-16 20:10:04 -0600

    login from firebase functionality

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -1,10 +1,48 @@
-export default function Login() {
-    return (
-        <section className='login-card'>
-            <form className='login-form'>
-                
-            </form>
-        </section>
-    )
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../../services/firebase'
 
+export default function Login() {
+
+    const [ email, setEmail ] = useState('')
+    const [ password, setPassword ] = useState('')
+    const navigate = useNavigate()
+
+    const signIn = (e: React.ChangeEvent<any>) => {
+        e.preventDefault()
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+        navigate('/')
+    }
+    return (
+        <main>
+            <section className='login-card'>
+                <form className='login-form' onSubmit={signIn}>
+                    <input 
+                        type="email"
+                        placeholder="Email"
+                        className="login-input"
+                        onChange={(e) => setEmail(e.target.value)}
+                        required/>
+                    <input 
+                        type="password"
+                        placeholder="Password"
+                        className="login-input"
+                        onChange={(e) => setPassword(e.target.value)}
+                        required/>
+                    <button type="submit" className="login-button">Log In</button>
+                </form>
+            </section>
+            <section className="signup-redirect">
+                <p>Don't have an account??</p>
+                <Link to="/signup" className="signup-link">Log In</Link>
+            </section>
+        </main>
+    )
 }
