a371d81ac50953c53547b5382fa486169de7631c
Eric Hardy <erichardy011@gmail.com>
2024-02-09 01:10:42 -0600

    search will successfully call lastFm api to search for the first 50 relevant albums

--- src/pages/New Album/NewAlbum.tsx
+++ src/pages/New Album/NewAlbum.tsx
@@ -1,7 +1,38 @@
+import { useState, useEffect } from 'react'
 export default function AddAlbum() {
-    
+
+    const [albumsFound, setAlbumsFound] = useState([])
+    const [album, setAlbum] = useState('')
+
+    const findAlbum = async (e) => {
+        e.preventDefault()
+        const fetchAlbum = await fetch(`https://ws.audioscrobbler.com/2.0/?method=album.search&album=${album}&api_key=${import.meta.env.VITE_LASTFM_API_KEY}&format=json`)
+        const albumData = await fetchAlbum.json()
+        setAlbumsFound(albumData.results.albummatches.album)
+    }
+    const listAlbums = albumsFound.map((album, index) => {
+        return (
+            <div className="album-list" key={index}>
+                <p>{album.name}</p>
+                <p>{album.artist}</p>
+            </div>
+        )
+    })
     return (
         <>
+            <section className="new-album-card">
+                <form className="find-album" onSubmit={findAlbum}>
+                    <input
+                        type="text"
+                        placeholder="Album Name"
+                        className="find-album-name"
+                        onChange={(e) => setAlbum(e.target.value)}/>
+                    <button type="submit">Find Album</button>
+                </form>
+                <div className="album-list">
+                    {listAlbums}
+                </div>
+            </section>
         </>
     )
 }
