4a991539ac3a4663c45c70b1c3796e33c0fcb187
Eric Hardy <erichardy011@gmail.com>
2024-02-10 03:00:59 -0600

    button on each album that keeps track of album in the state

--- src/pages/New Album/NewAlbum.tsx
+++ src/pages/New Album/NewAlbum.tsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 export default function AddAlbum() {
 
     const [albumsFound, setAlbumsFound] = useState([])
+    const [albumDetails, setAlbumDetails] = useState([])
     const [album, setAlbum] = useState('')
 
     const findAlbum = async (e) => {
@@ -10,11 +11,17 @@ export default function AddAlbum() {
         const albumData = await fetchAlbum.json()
         setAlbumsFound(albumData.results.albummatches.album)
     }
+
+    const addAlbumToList = (album) => {
+        setAlbumDetails(album)
+        console.log(albumDetails)
+    }
     const listAlbums = albumsFound.map((album, index) => {
         return (
             <div className="album-list" key={index}>
                 <p>{album.name}</p>
                 <p>{album.artist}</p>
+                <button className="add-album-btn" onClick={() => {addAlbumToList(album)}}>Add Album</button>
             </div>
         )
     })
