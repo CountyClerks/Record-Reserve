9e6031f1ddcc387f3114f8c36749138b6c4e5390
Eric Hardy <erichardy011@gmail.com>
2024-02-11 01:28:35 -0600

    searching for album now shows album cover art, album name, and artist of album

--- src/pages/New Album/NewAlbum.tsx
+++ src/pages/New Album/NewAlbum.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 export default function AddAlbum() {
 
     const [albumsFound, setAlbumsFound] = useState([])
-    const [albumDetails, setAlbumDetails] = useState([])
+    const [albumDetails, setAlbumDetails] = useState({})
     const [album, setAlbum] = useState('')
 
     const findAlbum = async (e) => {
@@ -13,12 +13,19 @@ export default function AddAlbum() {
     }
 
     const addAlbumToList = (album) => {
-        setAlbumDetails(album)
+        setAlbumDetails({
+            albumName: album.name,
+            albumArtist: album.artist,
+            albumImage: album.image[2]["#text"]
+        })
+        //Set album details {album name, album artist, album image}
+        //Save album details in firebase collection
         console.log(albumDetails)
     }
     const listAlbums = albumsFound.map((album, index) => {
         return (
             <div className="album-list" key={index}>
+                <img src={album.image[2]["#text"]} alt="Album image" />
                 <p>{album.name}</p>
                 <p>{album.artist}</p>
                 <button className="add-album-btn" onClick={() => {addAlbumToList(album)}}>Add Album</button>
