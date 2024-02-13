a1795b46b3846a2d7ece498d41cefcb7f01248e5
Eric Hardy <erichardy011@gmail.com>
2024-02-12 18:56:59 -0600

    display albums that were added to firebase database for each user

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useAuth } from '../../services/auth'
 import { db } from '../../services/firebase'
 import { ref, get, child } from 'firebase/database'
@@ -7,26 +7,59 @@ import { Link } from 'react-router-dom'
 
 export default function Home() {
     const currentUser = useAuth()
-    const [ displayName, setDisplayName ] = useState('')
+    const [ albumList, setAlbumList ] = useState([])
     const dbref = ref(db)
     const currentId = currentUser.authUser?.uid
-
-    // get(child(dbref, `users/${currentId}`)).then((snapshot) => {
-    //     const data = snapshot.val()
-    //     setDisplayName(data.username)
-    // })
-
-    // const profileName = () => {
-    //     return (
-    //         <h1>Current user is: {displayName ? displayName : ""}</h1>
-    //     )
-    // } 
+    let index
+    // useEffect(() => {
+    //     //READING NULL FOR SOME REASON
+    //     const getAlbumList = get(child(dbref, `users/${currentId}/albums`)).then((snapshot) => {
+    //             const data = snapshot.val()
+    //             console.log(data)
+    //             setAlbumList(data)
+    //         })
+    //     getAlbumList
+    // }, [])
+    const getAlbumList = get(child(dbref, `users/${currentId}/albums`)).then((snapshot) => {
+        const data = snapshot.val()
+        setAlbumList(data)
+    })
+    getAlbumList
+    const renderAlbums = albumList.map((albumList, index) => {
+        console.log(albumList)
+         return (
+            <div className="album-list" key={index}>
+                <img src={albumList.albumImage} alt="Album image" />
+                <p>{albumList.albumName}</p>
+                <p>{albumList.albumArtist}</p>
+            </div>
+        ) 
+    })
+    // function renderAlbums(albumList, index, currentUser) {
+    //     console.log(albumList)
+    //     if(currentUser == null) {
+    //         return(
+    //             <p>Hi</p>
+    //         )
+    //     } else {
+    //         getAlbumList
+    //         return (
+    //             <div className="album-list" key={index}>
+    //                 <img src={albumList[index].albumImage}/>
+    //                 <p>{albumList.albumName}</p>
+    //                 {/* <p>{albumList.albumArtist}</p> */}
+    //             </div>
+    //         )
+    //     }
+    // }
     return (
         <>
             <main>
                 <li>
                     <Link to='/new-album'>Add Album</Link>
                 </li>
+                {/* {renderAlbums(albumList, index, currentUser)} */}
+                {renderAlbums}
             </main>
         </>
     )
