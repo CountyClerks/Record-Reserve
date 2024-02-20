45e1d46b433e08d7ccb952b402f3b3adc9fce70a
Eric Hardy <erichardy011@gmail.com>
2024-02-19 20:26:27 -0600

    use formik for log in form

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -1,41 +1,65 @@
 import { Link, useNavigate } from 'react-router-dom'
-import { useState } from 'react'
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '../../services/firebase'
+import { useFormik } from 'formik'
 
 export default function Login() {
-
-    const [ email, setEmail ] = useState('')
-    const [ password, setPassword ] = useState('')
     const navigate = useNavigate()
 
-    const signIn = (e: React.ChangeEvent<any>) => {
-        e.preventDefault()
-        signInWithEmailAndPassword(auth, email, password)
+    const validate = values => {
+        const errors: any = {}
+        if(!values.email) {
+            errors.email = 'Required'
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+            errors.email = 'Invalid Email Address'
+        }
+
+        if(!values.password) {
+            errors.password = 'Required'
+        }
+        return errors
+    }
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        validate,
+        onSubmit: values => {
+            signInWithEmailAndPassword(auth, values.email, values.password)
             .then((userCredential) => {
             })
             .catch((error) => {
                 console.log(error)
             })
         navigate('/')
-    }
+        }
+    })
+
     return (
         <main>
             <section className='login-card'>
-                <form className='login-form' onSubmit={signIn}>
-                    <input 
+                <form onSubmit={formik.handleSubmit}>
+                    <label htmlFor="email">Email Address </label>
+                    <input
+                        id="email"
+                        name="email"
                         type="email"
-                        placeholder="Email"
-                        className="login-input"
-                        onChange={(e) => setEmail(e.target.value)}
-                        required/>
-                    <input 
+                        onChange={formik.handleChange}
+                        value={formik.values.email} 
+                    />
+                    {formik.errors.email ? <div>{formik.errors.email as string}</div> : null}
+                    <label htmlFor="password">Password </label>
+                    <input
+                        id="password"
+                        name="password"
                         type="password"
-                        placeholder="Password"
-                        className="login-input"
-                        onChange={(e) => setPassword(e.target.value)}
-                        required/>
-                    <button type="submit" className="login-button">Log In</button>
+                        onChange={formik.handleChange}
+                        value={formik.values.password} 
+                    />
+                    {formik.errors.password ? <div>{formik.errors.password as string}</div> : null}
+                    <button type="submit">Log In</button>
                 </form>
             </section>
             <section className="signup-redirect">
