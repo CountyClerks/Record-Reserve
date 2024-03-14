ba7859042ec2b7839baa0d6de002b7bdc75b8fcc
Eric Hardy <erichardy011@gmail.com>
2024-03-14 18:45:14 -0500

    change navbar html setup

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,8 +3,11 @@ import { auth } from "../services/firebase"
 import { signOut } from "firebase/auth"
 import { useNavigate } from "react-router-dom"
 import profileImg from "../images/user.svg"
+import { useState } from 'react'
+
 export default function Navbar() {
 
+    const [showMenu, setShowMenu] = useState(false)
     const navigate = useNavigate()
     const signingOut = (e: React.ChangeEvent<any>) => {
 
@@ -16,22 +19,29 @@ export default function Navbar() {
         })
         navigate('/')
     }
+
+    const handleNavClick = () => {
+        if(showMenu === true) {
+            setShowMenu(false)
+        } else if (showMenu === false) {
+            setShowMenu(true)
+        }
+    }
     return (
         <header>
             <nav>
-                <ul className="nav-list">
-                    <li>
-                        <Link to='/' className="nav-link">Home</Link>
-                    </li>
-                    <li>
-                        <img src={profileImg} className="profile-image"/>
-                    </li>
-                </ul>
-                <div className="hamburger-menu">
-                    <Link to='/signup' className="nav-link">Sign Up</Link>
-                    <Link to='/login' className="nav-link">Log In</Link>    
-                    <button type="button" className="log-out-btn" onClick={signingOut}>Log Out</button>
+                <div className="navbar">
+                    <Link to='/' className="nav-link">Record Reserve</Link>
+                    <div id="hamburger-menu">
+                        <img src={profileImg} className="profile-image" onClick={handleNavClick}/>
+                        <div className="nav-links" style={{display: showMenu ? 'block' : 'none'}}>
+                            <Link to='/signup' className="nav-link">Sign Up</Link>
+                            <Link to='/login' className="nav-link">Log In</Link>    
+                            <button type="button" className="log-out-btn" onClick={signingOut}>Log Out</button>
+                        </div>
+                    </div>
                 </div>
+                
             </nav>
         </header>
     )
