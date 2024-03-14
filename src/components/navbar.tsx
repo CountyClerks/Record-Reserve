6bba80d5d6fbb2d59b5c24b89cf4e90cf611c645
Eric Hardy <erichardy011@gmail.com>
2024-03-13 21:50:01 -0500

    change navbar to hamburger menu

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import { auth } from "../services/firebase"
 import { signOut } from "firebase/auth"
 import { useNavigate } from "react-router-dom"
+import profileImg from "../images/user.svg"
 export default function Navbar() {
 
     const navigate = useNavigate()
@@ -23,15 +24,14 @@ export default function Navbar() {
                         <Link to='/' className="nav-link">Home</Link>
                     </li>
                     <li>
-                        <Link to='/signup' className="nav-link">Sign Up</Link>
-                    </li>
-                    <li>
-                        <Link to='/login' className="nav-link">Log In</Link>
-                    </li>
-                    <li>
-                        <button type="button" className="log-out-btn" onClick={signingOut}>Log Out</button>
+                        <img src={profileImg} className="profile-image"/>
                     </li>
                 </ul>
+                <div className="hamburger-menu">
+                    <Link to='/signup' className="nav-link">Sign Up</Link>
+                    <Link to='/login' className="nav-link">Log In</Link>    
+                    <button type="button" className="log-out-btn" onClick={signingOut}>Log Out</button>
+                </div>
             </nav>
         </header>
     )
